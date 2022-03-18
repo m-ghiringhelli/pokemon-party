69c36f88f2bda60f1033478d143faced15283fe1
m-ghiringhelli <mghiring@gmail.com>
2022-03-17 17:32:53 -0700

    more alphabet

--- src/components/Alphabetical/Alphabetical.js
+++ src/components/Alphabetical/Alphabetical.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Alphabetical.css';
 
-export default function Alphabetical() {
+export default function Alphabetical({ setAlphabetized }) {
   
   return (
     <div>
-      <p className='sort'>A to Z</p>
+      <p className='sort' onClick={setAlphabetized}>A to Z</p>
       <p className='sort'>Z to A</p>
     </div>
   );
